3ed1074d53a8b1b243f6630b702cc1c93d43c743
Yash-Garg <ben10.yashgarg@gmail.com>
2020-07-09 01:46:42 +0530

    Fix script output

--- 1337x-scraper/main.py
+++ 1337x-scraper/main.py
@@ -19,37 +19,40 @@ if query != "":
         soup = BeautifulSoup(response.content, "lxml")
         urls = []
         data = []
-        titles = []
-        seeders = []
         table = soup.find(
             "table", attrs={"class": "table-list table table-responsive table-striped"})
         rows = table.find_all("tr")
         for row in rows:
             cols = row.find_all("td", attrs={"class": "coll-1 name"})
-            for seeds in row.find_all("td", attrs={"class": "coll-2 seeds"}):
-                seeders.append(seeds.contents[0])
             for col in cols:
                 torrents = col.find_all("a", href=True, class_=None)
-                headings = col.find("a", href=True, class_=None).contents[0]
                 for torrent in torrents:
                     urls.append(base_url + torrent["href"])
-                    titles.append(headings)
         for url in urls:
             resp = requests.post(url, headers=headers)
             if resp.status_code == 200:
                 soup = BeautifulSoup(resp.content, "lxml")
+                division = soup.find(
+                    "div", attrs={"class": "box-info-heading clearfix"})
+                title = division.find("h1", text=True, class_=None)
+                seeds = soup.find("span", attrs={"class": "seeds"})
                 size = soup.find("span", text=re.compile(
                     r'([0-9].[0-9]) ([A-Z])'), class_=None)
                 magnet = soup.find("a", href=re.compile(
                     r'[magnet]([a-z]|[A-Z])\w+'), class_=True).attrs["href"]
-                for title in titles:
-                    for seed in seeders:
-                        data.append("\nTitle: {}\nSeeders: {}\nSize: {}\nMagnet Link: {}\n".format(
-                            title, seed, size.contents[0], magnet))
+                data.append("\nTitle: {}\nSeeders: {}\nSize: {}\nMagnet Link: {}\n".format(
+                    title.contents[0], seeds.contents[0], size.contents[0], magnet))
         file = open("output.txt", "w", encoding="utf=-8")
         file.writelines(data)
         file.close()
         print("\nSuccessfully saved torrents to output.txt")
+        ch = input("\nWant to open output file? (Y/N): ")
+        if (ch == "Y" or ch == "y"):
+            print("\nOpening file! Please wait....")
+            os.startfile("output.txt")
+        else:
+            exit(0)
+
 else:
     print("\nNo keyword specified. Re-run the script!")
     exit(0)
